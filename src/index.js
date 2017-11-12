940a49c5bf1a35eb9cea2e0d33b133a4c9b917fb
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-11-12 19:30:05 +0530

    Working react component base template

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>This is my updated new component</div>
+    );
+  }
+}
+export default MyComponent;
